2e778efd22060088495dba11178c7f4bc24d5128
Carlos Garate <cgarate@carlostwlaptop>
2020-06-11 01:40:15 -0400

    update props

--- src/widgets/loaders/circle-loader/circle-loader.component.js
+++ src/widgets/loaders/circle-loader/circle-loader.component.js
@@ -3,14 +3,14 @@ import ContentLoader from 'react-content-loader'
 
   export const CircleLoader = (
     {
-      primaryColor = '#f35184',
-      secondaryColor= '#ec8f76',
+      backgroundColor = '#f35184',
+      foregroundColor= '#ec8f76',
     }) => <ContentLoader
 		height={160}
 		width={300}
 		speed={3}
-		primaryColor={primaryColor}
-		secondaryColor={secondaryColor}
+		backgroundColor={backgroundColor}
+		foregroundColor={foregroundColor}
     style={{width: '100%', height: '10rem', margin: '3rem 0 0 0'}}
 	>
 		<circle cx="150" cy="80" r="80" />
--- stories/index.js
+++ stories/index.js
@@ -4,5 +4,5 @@ import { storiesOf } from "@storybook/react";
 import { CircleLoader } from "../src/widgets";
 
 storiesOf("CircleLoader", module).add("Red to Yellow", () => (
-  <CircleLoader primaryColor={"#ff0000"} secondaryColor={"yellow"} />
+  <CircleLoader backgroundColor="#ff0000" foregroundColor="yellow" />
 ));
