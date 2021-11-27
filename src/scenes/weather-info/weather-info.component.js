368f0e61701da0e50fcd1af9a6d72b52db1c4633
Carlos Garate <carlos.m.garate@gmail.com>
2021-11-27 03:58:58 -0500

    format

--- src/scenes/weather-info/weather-info.component.js
+++ src/scenes/weather-info/weather-info.component.js
@@ -1,54 +1,59 @@
-import React from 'react';
+import React from "react";
 
 import { CitySelector, CircleLoader, Error } from "../../widgets";
 import { Block, Main } from "../../components";
-import { WeatherCard } from './components';
+import { WeatherCard } from "./components";
 
-export const WeatherInfo = (
-  {
-    isFetching,
-    weatherConditions,
-    basicWeatherData,
-    submitHandler,
-    inputTextSearchHandler,
-    errorState,
-  }) => {
-    // The spinner
-    if (isFetching) {
-      return <CircleLoader />;
-    }
-
-    const renderInputSearchBox = () => (
-      <CitySelector
-        submitHandler={submitHandler}
-        inputTextSearchHandler={inputTextSearchHandler}
-      />
-    )
-
-    // Initial state when data has not been retrieved
-    if (errorState.message) {
-      return (
-        <Main flex flex_column items_center justify_center pa3>
-          {renderInputSearchBox()}
-          {(errorState.message ? <Error errorMessage={errorState.message} /> : null)}
-        </Main>
-      );
-    }
-
-    // Data fetching was successful.
+export const WeatherInfo = ({
+  isFetching,
+  weatherConditions,
+  basicWeatherData,
+  submitHandler,
+  inputTextSearchHandler,
+  errorState,
+}) => {
+  // The spinner
+  if (isFetching) {
     return (
       <Main flex flex_column items_center justify_center pa3>
-        <Block flex flex_column items_start>
-          {renderInputSearchBox()}
-          <WeatherCard
-            currentConditions={weatherConditions}
-            temp={basicWeatherData.temp}
-            humidity={basicWeatherData.humidity}
-            location={basicWeatherData.location}
-            country={basicWeatherData.country}
-            // clickHandler={this.weatherClickHandler}
-          />
-        </Block>
+        <CircleLoader />
       </Main>
     );
-}
+  }
+
+  const renderInputSearchBox = () => (
+    <CitySelector
+      submitHandler={submitHandler}
+      inputTextSearchHandler={inputTextSearchHandler}
+    />
+  );
+
+  // Initial state when data has not been retrieved
+  if (errorState.message) {
+    return (
+      <Main flex flex_column items_center justify_center pa3>
+        {renderInputSearchBox()}
+        {errorState.message ? (
+          <Error errorMessage={errorState.message} />
+        ) : null}
+      </Main>
+    );
+  }
+
+  // Data fetching was successful.
+  return (
+    <Main flex flex_column items_center justify_center pa3>
+      <Block flex flex_column items_start>
+        {renderInputSearchBox()}
+        <WeatherCard
+          currentConditions={weatherConditions}
+          temp={basicWeatherData.temp}
+          humidity={basicWeatherData.humidity}
+          location={basicWeatherData.location}
+          country={basicWeatherData.country}
+          // clickHandler={this.weatherClickHandler}
+        />
+      </Block>
+    </Main>
+  );
+};
